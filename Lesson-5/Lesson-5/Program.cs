b36609d3445f9bd9e9bfb2d1434f14c71deaaabb
Vladimir Lyashkov <vladimir.lyashkov14@yandex.ru>
2021-03-14 23:08:22 +0900

    ДЗ к 5 уроку

---
+++ Lesson-5/Lesson-5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Реализуйте DFS и BFS для дерева с выводом каждого шага в консоль.
+        }
+    }
+}
