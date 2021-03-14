107362f41270b30718d6d61dcac11724aed7ea63
Vladimir Lyashkov <vladimir.lyashkov14@yandex.ru>
2021-03-14 23:10:28 +0900

    ДЗ к 6 уроку

---
+++ Lesson-6/Lesson-6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Модифицируйте DFS и BFS из предыдущего урока, но уже для графа, также с выводом каждого шага
+        }
+    }
+}
